aebae63d985e7dc173a586eef1f12b95189682d8
Madhvi Malviya <madhvimalviya1@gmail.com>
2023-05-11 16:08:43 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,15 @@ import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import FilterListIcon from '@mui/icons-material/FilterList'; 
 import ShareIcon from '@mui/icons-material/Share';
 import GroupIcon from '@mui/icons-material/Group';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 
 function Navbar() {
     return (
         <div className={style.Nav} >
             <ul className={style.mainNav} >
                 <h2>Home Task Management</h2>
-                <li> <GroupIcon />Workplace Visible</li>
-                <button>Board</button>
+                <li> <GroupIcon /> Workplace Visible</li>
+                <button><SportsEsportsIcon/>  Board</button>
                 <li> <TrendingUpIcon /> power ups</li>
                 <li><BoltIcon />Automation</li>
                 <li> <FilterListIcon /> Filter</li>
